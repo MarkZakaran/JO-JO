9b637cec6a521c1422df50b2d832d12e64e8990a
MarkZakaran <124691413+MarkZakaran@users.noreply.github.com>
2023-02-07 17:22:27 +0300

    слава сталину

---
+++ main.py
@@ -0,0 +1,18 @@
+from random import randrange
+number = randrange(100)
+
+guesses_taken = 0
+while guesses_taken < 10:
+    guess = int(input("Ddtlbnt xbckj:"))
+    if guess < number:
+        print("Ваше число меньше")
+    elif guess > number:
+        print("Dfit xbckj ,jkmit")
+    elif guess == number:
+        print("Ds gj,tlbkb")
+        if guesses_taken += 3
+
+        if guesses_taken == 10:
+            print('DS ghjbuhfkb')
+            print(number)
+
